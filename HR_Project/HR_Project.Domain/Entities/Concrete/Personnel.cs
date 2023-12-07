d6349a732d117acfe4586c5b1413014a15a5e649
Osman eser <osmaneser513@live.com>
2023-12-07 11:57:34 +0300

    ID:6 baclog is completed

---
+++ HR_Project/HR_Project.Domain/Entities/Concrete/Personnel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Project.Domain.Entities.Concrete
+{
+	public class Personnel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string? Address { get; set; }
+		public string? City { get; set; }
+		public string? Region { get; set; }
+		public DateTime? BirthDate { get; set; }
+		public DateTime? HireDate { get; set; }
+		//Todo: Add Bloodtype enum
+        //public Bloodtype? Bloodtype { get; set; }
+
+        public int? DepartmentId { get; set; }
+		public Department Department { get; set; }
+
+		public int? ManagerId { get; set; }
+		public Personnel Manager { get; set; }
+
+		public ICollection<Personnel> Subordinates { get; set; }
+		//public ICollection<Salary> Salaries { get; set; }
+		public ICollection<Absence> Absences { get; set; }
+		public ICollection<Advance> Advance { get; set; }
+
+	}
+}

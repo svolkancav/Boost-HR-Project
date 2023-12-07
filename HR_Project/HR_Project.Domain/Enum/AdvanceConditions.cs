0e70a1c03de10ae400676e6826a09ed96e896b62
scetin23 <sultancetin2022@gmail.com>
2023-12-07 14:22:04 +0300

    Avans ve izin enumları oluşturuldu.

---
+++ HR_Project/HR_Project.Domain/Enum/AdvanceConditions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HR_Project.Domain.Enum
+{
+    public enum AdvanceConditions
+    {
+        [Display(Name = "Onaylandı")]
+        Approved =1,
+        [Display(Name = "Beklemede")]
+        Pending =2,
+        [Display(Name = "Reddedildi")]
+        Refused =3
+    }
+}

2d3920a9f33d5f2e71b233041536ba39e900fb08
scetin23 <sultancetin2022@gmail.com>
2023-12-08 09:42:29 +0300

    BloodType enum eklendi.

---
+++ HR_Project/HR_Project.Domain/Enum/BloodType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Project.Domain.Enum
+{
+    public enum BloodType
+    {
+        ZeroRhPositive=1,
+        ZeroRhNegative=2,
+        ABRhPositive=3,
+        ABRhNegative =4,
+        ARhPositive = 5,
+        ARhNegative = 6,
+        BRhPositive = 7,
+        BRhNegative = 8
+    }
+}

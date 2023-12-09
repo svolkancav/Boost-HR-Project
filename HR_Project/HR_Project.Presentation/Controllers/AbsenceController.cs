fa80c5f876f1407b2d38deec2719b3ff9f410b21
Osman eser <osmaneser513@live.com>
2023-12-09 13:09:57 +0300

    abcense controller dolduruldu

--- HR_Project/HR_Project.Presentation/Controllers/AbsenceController.cs
+++ HR_Project/HR_Project.Presentation/Controllers/AbsenceController.cs
@@ -1,4 +1,6 @@
-﻿using HR_Project.Presentation.APIService;
+﻿using HR_Project.Common.Models.DTOs;
+using HR_Project.Common.Models.VMs;
+using HR_Project.Presentation.APIService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HR_Project.Presentation.Controllers
@@ -11,10 +13,45 @@ namespace HR_Project.Presentation.Controllers
         {
             _apiService = apiService;
         }
-
-        public IActionResult Index()
+        // listelemek için
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<AbsenceVM> absences =await _apiService.GetAsync<List<AbsenceVM>>("absence", HttpContext.Request.Cookies["access-token"]);
+
+            return View(absences);
         }
+
+        // ekleme için
+        public IActionResult Create()
+        {
+			return View();
+		}
+        [HttpPost]
+		public async Task<IActionResult> Create(AbsenceDTO model)
+        {
+			await _apiService.PostAsync<AbsenceDTO,AbsenceDTO>("absence", model, HttpContext.Request.Cookies["access-token"]); //_apiService.PostAsync<AbsenceDTO,AbsenceDTO>... ikinci tip geri dönüş tipi. Ama API den geri dönüş modeli göndermiyoruz.
+			return RedirectToAction("Index");
+		}
+
+		// güncelleme için
+		public async Task<IActionResult> Update(int id)
+        {
+			AbsenceVM absence = await _apiService.GetAsync<AbsenceVM>($"absence/{id}", HttpContext.Request.Cookies["access-token"]);
+			return View(absence);
+		}
+
+        [HttpPost]
+		public async Task<IActionResult> Update(AbsenceDTO model)
+        {
+			await _apiService.UpdateAsync<AbsenceDTO>("absence", model, HttpContext.Request.Cookies["access-token"]);
+			return RedirectToAction("Index");
+		}
+
+		// silme için
+		public async Task<IActionResult> Delete(int id)
+        {
+			await _apiService.DeleteAsync<AbsenceDTO>($"absence",id, HttpContext.Request.Cookies["access-token"]);
+			return RedirectToAction("Index");
+		}
     }
 }

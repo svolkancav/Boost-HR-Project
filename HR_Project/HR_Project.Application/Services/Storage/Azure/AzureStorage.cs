1ad8cf8ecd6b2485e59072d7523c70ec0c4e64fc
Osman eser <osmaneser513@live.com>
2023-12-15 19:23:05 +0300

    dosya işlemleri içini servis oluşturuldu ama kullanıma alınmadı

---
+++ HR_Project/HR_Project.Application/Services/Storage/Azure/AzureStorage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using HR_Project.Domain.Services.StorageService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace HR_Project.Application.Services.Storage.Azure
+{
+    public class AzureStorage : Storage, IAzureStorage
+    {
+		readonly BlobServiceClient _blobServiceClient;
+		BlobContainerClient _blobContainerClient;
+		public AzureStorage(IConfiguration configuration)
+		{
+			_blobServiceClient = new(configuration["Storage:Azure"]);
+		}
+		public async Task DeleteAsync(string containerName, string fileName)
+		{
+			_blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+			BlobClient blobClient = _blobContainerClient.GetBlobClient(fileName);
+			await blobClient.DeleteAsync();
+		}
+
+		public List<string> GetFiles(string containerName)
+		{
+			_blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+			return _blobContainerClient.GetBlobs().Select(b => b.Name).ToList();
+		}
+
+		public bool HasFile(string containerName, string fileName)
+		{
+			_blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+			return _blobContainerClient.GetBlobs().Any(b => b.Name == fileName);
+		}
+
+		public async Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection files)
+		{
+
+			_blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+			await _blobContainerClient.CreateIfNotExistsAsync();
+			await _blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+			List<(string fileName, string path)> datas = new List<(string fileName, string path)>();
+
+			foreach (var file in files)
+			{
+
+				var fileNewName = await FileRenameAsync(containerName, file.FileName, HasFile);
+
+				BlobClient blobClient = _blobContainerClient.GetBlobClient(fileNewName);
+				await blobClient.UploadAsync(file.OpenReadStream());
+				datas.Add((file.Name, containerName));
+			}
+
+			return datas;
+		}
+	}
+}
---
+++ HR_Project/HR_Project.Application/Services/Storage/Storage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR_Project.Application.Operations;
+
+namespace HR_Project.Application.Services.Storage
+{
+    public class Storage
+    {
+        protected delegate bool HasFile(string pathOrContainerName, string fileName);
+        protected async Task<string> FileRenameAsync(string pathOrContainerName, string fileName, HasFile hasFileMethod, bool first = true)
+        {
+            string newFileName = await Task.Run(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+
+                string newFileName = string.Empty;
+
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+
+                    newFileName = NameOperation.CharacterRegulatory(oldName) + extension;
+                }
+                else
+                {
+                    newFileName = fileName;
+
+                    int index1 = newFileName.LastIndexOf('-');
+                    int index2 = newFileName.IndexOf(".");
+
+                    string number = newFileName.Substring(index1 + 1, index2 - index1 - 1);
+
+
+
+                    if (index1 == -1 || !int.TryParse(number, out int numberInt))
+                    {
+                        newFileName = Path.GetFileNameWithoutExtension(fileName) + "-2" + extension;
+                    }
+                    else
+                    {
+                        numberInt++;
+
+                        newFileName = newFileName.Remove(index1 + 1, index2 - index1 - 1).Insert(index1 + 1, numberInt.ToString());
+                    }
+
+                }
+
+                //if (File.Exists($"{pathOrContainerName}\\{newFileName}"))
+                if (hasFileMethod(pathOrContainerName, newFileName))
+                {
+                    return await FileRenameAsync(pathOrContainerName, newFileName, hasFileMethod, false);
+                }
+                else
+                    return newFileName;
+            });
+            return newFileName;
+        }
+    }
+}

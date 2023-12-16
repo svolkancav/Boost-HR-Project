47d855a07d77024ed827967b7086b80f2ec3a726
scetin23 <sultancetin2022@gmail.com>
2023-12-16 17:44:27 +0300

    ExpenseConfig , IExpenseRepository ve ExpenseRepository oluşturuldu.

---
+++ HR_Project/HR_Project.Domain/Repositories/IExpenseRepository.cs
@@ -0,0 +1,16 @@
+﻿using HR_Project.Domain.Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Project.Domain.Repositories
+{
+    public interface IExpenseRepository : IBaseRepository<Expense>
+    {
+        
+    }
+}

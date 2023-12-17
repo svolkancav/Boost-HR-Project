c4321829110534e77d3e2ad12a894957da146d0e
scetin23 <sultancetin2022@gmail.com>
2023-12-17 23:46:02 +0300

    Enumlar Türkçe'ye çevrildi, Profil ad soyadı değiştirilemez yapıldı.

--- HR_Project/HR_Project.Common/Models/DTOs/RegisterDTO.cs
+++ HR_Project/HR_Project.Common/Models/DTOs/RegisterDTO.cs
@@ -1,20 +1,70 @@
-﻿using System;
+﻿using HR_Project.Domain.Entities.Concrete;
+using HR_Project.Domain.Enum;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HR_Project.Common.Models.DTOs
 {
     public class RegisterDTO
     {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Title { get; set; }
-        public string PhoneNumber { get; set; }
+		[Display(Name = "Kullanıcı Adı")]
+		[Required(ErrorMessage = "Bu alan boş bırakılamaz.")]
+		public string UserName { get; set; }
 
-    }
+
+		[Display(Name = "Email")]
+		[Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+		public string Email { get; set; }
+
+
+		[Display(Name = "Parola")]
+		[Required(ErrorMessage = "Parola alanı boş bırakılamaz.")]
+		public string Password { get; set; }
+
+
+		[Display(Name = "Adı")]
+		[Required(ErrorMessage = "Adı alanı boş bırakılamaz.")]
+		public string Name { get; set; }
+
+
+		[Display(Name = "Soyadı")]
+		[Required(ErrorMessage = "Soyadı alanı boş bırakılamaz.")]
+		public string Surname { get; set; }
+
+
+		[Display(Name = "Ünvan")]
+		[Required(ErrorMessage = "Ünvan alanı boş bırakılamaz.")]
+		public string Title { get; set; }
+
+
+		[Display(Name = "Telefon Numarası")]
+		[Required(ErrorMessage = "Telefon numarası alanı boş bırakılamaz.")]
+		public string PhoneNumber { get; set; }
+
+
+		[Display(Name = "Şehir")]
+		[Required(ErrorMessage = "Şehir alanı boş bırakılamaz.")]
+		public string City { get; set; }
+
+
+		[Display(Name = "İlçe")]
+		[Required(ErrorMessage = "İlçe alanı boş bırakılamaz.")]
+		public Region Region { get; set; }
+
+
+		[Display(Name = "Kan Grubu")]
+		[Required(ErrorMessage = "Kan Grubu alanı boş bırakılamaz.")]
+		public BloodType BloodType { get; set; }
+
+
+		[Display(Name = "Doğum Günü")]
+		[Required(ErrorMessage = "Doğum Günü alanı boş bırakılamaz.")]
+		public DateTime BirthDate { get; set; }
+
+	}
 }
---
+++ HR_Project/HR_Project.Common/Models/VMs/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using HR_Project.Domain.Entities.Concrete;
+using HR_Project.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Project.Common.Models.VMs
+{
+	public class RegisterVM
+	{
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Title { get; set; }
+		public string PhoneNumber { get; set; }
+		public string City { get; set; }
+		public Region Region { get; set; }
+		public BloodType BloodType { get; set; }
+		public DateTime BirthDate { get; set; }
+	}
+}
